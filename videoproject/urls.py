25f857427890d277374fe1f6014d9c895ed3f307
jany <lrnboy@gmail.com>
2019-03-22 03:39:39 +0800

    heroku提交

--- videoproject/urls.py
+++ videoproject/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from video import views
-
+admin.autodiscover()
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('video/',include('video.urls')),
