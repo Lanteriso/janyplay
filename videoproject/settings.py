5106ecd2c71ec974425be97705dfd6166032a56b
jany <lrnboy@gmail.com>
2019-03-23 01:53:14 +0800

    heroku提交

--- videoproject/settings.py
+++ videoproject/settings.py
@@ -136,7 +136,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'zh_Hans'
+LANGUAGE_CODE = 'zh_hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
